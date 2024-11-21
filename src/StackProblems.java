008fae4e20d3a576fe024902ac137a244ed07492
Andres Ardila <andres.ardila.agudelo@gmail.com>
2024-11-21 02:43:56 -0500

    ERPM

--- src/StackProblems.java
+++ src/StackProblems.java
@@ -1,12 +1,111 @@
+import java.util.Queue;
+import java.util.Stack;
+
 public class StackProblems {
 
     public static void main(String[] args) {
-        StackProblems s = new StackProblems();
+        StackProblems sta = new StackProblems();
 
 
+        //Valid Parentheses
+        //String s= "()";
+        //System.out.println(sta.isValid(s));
+
+        //Min Stack
+        //MinStack ms = new MinStack();
+        //ms.push(-2);
+        //ms.push(0);
+        //ms.push(-3);
+        //System.out.println(ms.getMin());
+        //ms.pop();
+        //System.out.println(ms.top());
+        //System.out.println(ms.getMin());
+
+        //Evaluate Reverse Polish Notation
+        //String[] tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        //System.out.println(sta.evalRPN(tokens));
 
     }
+    //Valid Parentheses
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
 
+        for(char c : s.toCharArray()) {
+            if(c == '{' || c == '[' || c == '(') {
+                st.push(c);
+            }else if(c == '}' || c == ']' || c == ')') {
+                if(st.isEmpty()) {
+                    return false;
+                }
+                char cp=st.pop();
+                if(cp=='{'&&c!='}')return false;
+                if(cp=='['&&c!=']')return false;
+                if(cp=='('&&c!=')')return false;
+            }
+        }
+        if(!st.isEmpty()) return false;
+        return true;
+    }
 
+    //Min Stack
+    static class MinStack {
+        Stack<Integer> st;
+        Stack<Integer> minS;
+        int min;
+        public MinStack() {
+            st=new Stack<>();
+            minS=new Stack<>();
 
+        }
+
+        public void push(int val) {
+            st.push(val);
+            if(minS.isEmpty()||val<=minS.peek()) minS.push(val);
+        }
+
+        public void pop() {
+            if(st.isEmpty()) return;
+
+            int top =st.pop();
+            if(top==minS.peek()) minS.pop();
+        }
+
+        public int top() {
+            return st.peek();
+        }
+
+        public int getMin() {
+            return minS.peek();
+        }
+    }
+
+    //Evaluate Reverse Polish Notation
+    public int evalRPN(String[] tokens) {
+        if(tokens==null || tokens.length==0) return 0;
+        Stack<Integer> st=new Stack<>();
+        int a,b;
+        for(String token : tokens) {
+            if(token.equals("+")){
+                b=st.pop();
+                a=st.pop();
+                st.push(a+b);
+            }else if(token.equals("-")){
+                b=st.pop();
+                a=st.pop();
+                st.push(a-b);
+            }else if(token.equals("*")){
+                b=st.pop();
+                a=st.pop();
+                st.push(a*b);
+            }else if(token.equals("/")){
+                b=st.pop();
+                a=st.pop();
+                st.push(a/b);
+            }else{
+                st.push(Integer.parseInt(token));
+            }
+        }
+        return st.peek();
+
+    }
 }
